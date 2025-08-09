bcc4cdd79c11a267930faa4ec37adc2503afba53
Mustafa Kaan <MustafaKaanYavuz@outlook.com>
2025-08-09 14:43:12 +0300

    ignoreDuringBuilds added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // ... diğer ayarlarınız
+};
 
 export default nextConfig;
